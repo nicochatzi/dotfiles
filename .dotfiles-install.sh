1dc65bc19f3b5cbe9777d9279317e14a960cca39
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-01-23 13:51:52 +0000

    fix install script

--- .dotfiles-install.sh
+++ .dotfiles-install.sh
@@ -4,35 +4,54 @@ set -u
 
 OS=$(uname -s)
 
+SUDO=sudo
+
+if ! command -v $SUDO &> /dev/null
+then
+    SUDO=
+fi
+
 install_system_packages() {
-    packages_to_install=$@
+    packages_to_install=("$@")
 
     for package in "${packages_to_install[@]}"
     do
         if [[ $OS == "Darwin" ]]; then
             brew install "$package"
         else
-            sudo apt install "$package" -y
+            $SUDO apt install "$package" -y
         fi
     done
 }
 
+install_base_packages() {
+    base_packages=(\
+        curl \
+        git \
+        python3 \
+    )
+
+    install_system_packages "${base_packages[@]}"
+
+    if ! [[ $OS == "Darwin" ]]; then
+        $SUDO apt install -y python3-pip
+    fi
+}
+
 install_languages() {
-    lang_packages=(
-        python3
-        pip3
-        node
-        n
+    lang_packages=(\
+        node \
+        n \
     )
 
-    linters_packages=(
-        clang-format
-        hadolint
-        shellcheck
+    linters_packages=(\
+        clang-format \
+        hadolint \
+        shellcheck \
     )
 
-    install_system_packages $lang_packages
-    install_system_packages $linters_packages
+    install_system_packages "${lang_packages[@]}"
+    install_system_packages "${linters_packages[@]}"
 
     # Rust
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
@@ -45,27 +64,27 @@ install_languages() {
 }
 
 install_tui() {
-    tool_packages=(
-        just
-        exa
-        tig
-        nvim
-        bat
-        fd
-        procs
+    tool_packages=(/
+        just/
+        exa/
+        tig/
+        nvim/
+        bat/
+        fd/
+        procs/
         dust
-        bottom
-        zplug
-        git-delta
-        ripgrep
-        watchexec
-        sccache
-        gh
-        circleci
-        tmux
+        bottom/
+        zplug/
+        git-delta/
+        ripgrep/
+        watchexec/
+        sccache/
+        gh/
+        circleci/
+        tmux/
     )
 
-    install_system_packages $tool_packages
+    install_system_packages "${tool_packages[@]}"
 
     # Prezto
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
@@ -121,10 +140,10 @@ install_dotfiles() {
             config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
     fi;
 
-    dotfiles checkout
-    dotfiles config status.showUntrackedFiles no
+    dotfiles checkout dotfiles config status.showUntrackedFiles no
 }
 
+INSTALL_SYS=0
 INSTALL_LANUGAGES=0
 INSTALL_TUI=0
 INSTALL_DOTFILES=0
@@ -135,19 +154,24 @@ while [[ $# -gt 0 ]]; do
             INSTALL_LANUGAGES=1
             INSTALL_TUI=1
             INSTALL_DOTFILES=1
-            shift 2
+            INSTALL_SYS=1
+            shift
+            ;;
+        -s|--system)
+            INSTALL_SYS=1
+            shift
             ;;
         -l|--langs)
             INSTALL_LANUGAGES=1
-            shift 2
+            shift
             ;;
         -t|--terminal)
             INSTALL_TUI=1
-            shift 2
+            shift
             ;;
         -d|--dotfiles)
             INSTALL_DOTFILES=1
-            shift 2
+            shift
             ;;
         *)
             echo "Unknown option $1"
@@ -156,22 +180,27 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [ $INSTALL_TUI -eq 1 ] || [ $INSTALL_DOTFILES -eq 1 ] || [ $INSTALL_LANUGAGES -eq 1 ]; then
+if [ $INSTALL_SYS -eq 1 ] || [ $INSTALL_TUI -eq 1 ] || [ $INSTALL_DOTFILES -eq 1 ] || [ $INSTALL_LANUGAGES -eq 1 ]; then
     if [[ $OS == "Darwin" ]]; then
         brew update
     else
-        sudo apt update
+        $SUDO apt update
     fi
 fi
 
-if [ $INSTALL_TUI -eq 1 ]; then
-    echo "~> Installing terminal UI tools"
-    install_tui
+if [ $INSTALL_SYS -eq 1 ]; then
+    echo "~> Installing system packages"
+    install_base_packages
 fi
 
 if [ $INSTALL_LANUGAGES -eq 1 ]; then
     echo "~> Installing languages"
     install_languages
+fi
+
+if [ $INSTALL_TUI -eq 1 ]; then
+    echo "~> Installing terminal UI tools"
+    install_tui
 fi
 
 if [ $INSTALL_DOTFILES -eq 1 ]; then
