fef64d4ca575e819b281392b6f1055ee1eb79346
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-07-19 08:08:35 +0100

    disable foldcolumn indicator

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -113,5 +113,10 @@ return {
     'kevinhwang91/nvim-ufo',
     dependencies = 'kevinhwang91/promise-async',
     event = 'VeryLazy',
+    opts = {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { 'treesitter', 'indent' }
+      end
+    }
   }
 }
