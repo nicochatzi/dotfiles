f9132f97154ec88556687895e4ab8b05082a1fb7
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-09-27 17:38:04 +0100

    add another safari webapp hotkey

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -14,3 +14,8 @@ keys.setup_hotkey_macos({
     mods = { "alt" },
     key = '3'
 }, 'com.apple.Safari.WebApp.7E7AB543-CC1C-40CA-8943-87A42825EB3A')
+
+keys.setup_hotkey_macos({
+    mods = { "alt" },
+    key = '4'
+}, 'com.apple.Safari.WebApp.42CFC3A7-A873-4F70-AA0D-1640F387A562')
