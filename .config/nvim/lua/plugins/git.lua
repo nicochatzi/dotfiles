b74c2855749307d4e1489a2c220a3138440bb008
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-11-24 15:14:57 +0000

    back to jetbrains + git blame in neovim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -8,6 +8,14 @@ return {
         'lewis6991/gitsigns.nvim',
         event = { "BufReadPre", "BufNewFile" },
         opts = {
+            current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+                delay = 1000,
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+            },
             -- See `:help gitsigns.txt`
             signs = {
                 add = { text = '+' },
