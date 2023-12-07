b628483995c47f13f9a739c7f7da3e2de0626a4a
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-12-07 11:14:08 +0000

    add nvim-spectre split openning mappings

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -30,6 +30,27 @@ return {
     -- global search and replace utils
     'nvim-pack/nvim-spectre',
     cmd = 'Spectre',
+    config = function()
+      require("spectre").setup {
+        mapping = {
+          ['open_in_vsplit'] = {
+            map = "<c-v>",
+            cmd = "<cmd>lua vim.cmd('vsplit ' .. require('spectre.actions').get_current_entry().filename)<CR>",
+            desc = "open in vertical split"
+          },
+          ['open_in_split'] = {
+            map = "<c-x>",
+            cmd = "<cmd>lua vim.cmd('split ' .. require('spectre.actions').get_current_entry().filename)<CR>",
+            desc = "open in horizontal split"
+          },
+          ['open_in_tab'] = {
+            map = "<c-t>",
+            cmd = "<cmd>lua vim.cmd('tab split ' .. require('spectre.actions').get_current_entry().filename)<CR>",
+            desc = "open in new tab"
+          },
+        }
+      }
+    end,
   },
 
   {
