6327318d18bc1c37fedd778fcf7fd6120f856fc1
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-12-18 17:21:32 +0000

    align colors alacritty with kitty - default to alacritty

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,50 +1,49 @@
 local hotkey = require('lib/hotkey')
 local winman = require('lib/winman')
 
--- app_name = 'Alacritty',
--- bundle_id = 'org.alacritty',
+hotkey.setup {
+  mods = { "alt" },
+  key = '1',
+  app_name = 'Alacritty',
+  bundle_id = 'org.alacritty',
+  -- app_name = 'Kitty',
+  -- bundle_id = 'net.kovidgoyal.kitty',
+  scaled = true,
+}
 
 hotkey.setup {
-    mods = { "alt" },
-    key = '1',
-    app_name = 'Kitty',
-    bundle_id = 'net.kovidgoyal.kitty',
-    scaled = true,
-}
-
-hotkey.setup {
-    mods = { "alt" },
-    key = '2',
-    app_name = 'Arc',
-    bundle_id = 'company.thebrowser.Browser',
+  mods = { "alt" },
+  key = '2',
+  app_name = 'Arc',
+  bundle_id = 'company.thebrowser.Browser',
 }
 
 winman.setup {
-    mods = { "ctrl", "alt" },
-    key = 'Left',
-    action = winman.to_left_half
+  mods = { "ctrl", "alt" },
+  key = 'Left',
+  action = winman.to_left_half
 }
 
 winman.setup {
-    mods = { "ctrl", "alt" },
-    key = 'Right',
-    action = winman.to_right_half
+  mods = { "ctrl", "alt" },
+  key = 'Right',
+  action = winman.to_right_half
 }
 
 winman.setup {
-    mods = { "ctrl", "alt" },
-    key = 'Up',
-    action = winman.to_fullscreen
+  mods = { "ctrl", "alt" },
+  key = 'Up',
+  action = winman.to_fullscreen
 }
 
 winman.setup {
-    mods = { "ctrl", "alt" },
-    key = 'Down',
-    action = winman.to_original_frame
+  mods = { "ctrl", "alt" },
+  key = 'Down',
+  action = winman.to_original_frame
 }
 
 require('lib/clipmem').setup {
-    history = 10,
+  history = 10,
 }
 
 -- require('lib/spotify').launch_notifier()
