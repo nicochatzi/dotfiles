a11743e35a7e29b02ab98cbc032be04c47dc110f
nicochatzi <nico@htz.dev>
2024-02-12 11:15:41 +0100

    add i3/dunst battery check util

---
+++ .config/i3/battery-check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true; do
+    battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+
+    if [ "$battery_level" -lt 20 ]; then
+        dunstify -u critical -h string:x-dunst-stack-tag:battery "Low Battery" "Battery level is low: ${battery_level}%"
+    fi
+
+    sleep 10
+done
+
