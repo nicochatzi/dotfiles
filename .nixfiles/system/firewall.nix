e75738fe009b4645391006cbb70f82fe9f4f4a4e
nicochatzi <nico@htz.dev>
2024-02-13 23:34:38 +0100

    fix nix firewall config

--- .nixfiles/system/firewall.nix
+++ .nixfiles/system/firewall.nix
@@ -6,9 +6,6 @@
     allowPing = true;
     logRefusedConnections = true;
 
-    defaultIncomingPolicy = "drop";
-    defaultOutgoingPolicy = "accept";
-
     allowedTCPPorts = [
       22 # SSH
       # 80 # HTTP
@@ -17,14 +14,6 @@
       # 5900 # VNC
     ];
     allowedUDPPorts = [ ];
-
-    # Reject packets that appear to come from
-    # the outside but claim to be from a local IP
-    rejectPacketsWithLocalSource = true;
-
-    # Additional rules can be defined here
-    # Example: Allow a specific IP
-    # extraCommands = "iptables -A INPUT -s 192.168.1.100 -j ACCEPT";
   };
 }
 
