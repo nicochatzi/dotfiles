18e04dfd36a9d88d00be17a8a8d595d53ea392ce
nicochatzi <nico@htz.dev>
2024-02-16 14:07:02 +0100

    add bluetooth service

---
+++ .nixfiles/services/bluetooth.nix
@@ -0,0 +1,31 @@
+# https://nixos.wiki/wiki/Bluetooth
+
+{ pkgs, ... }:
+
+{
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+  hardware.pulseaudio.extraConfig = "
+    load-module module-switch-on-connect
+  ";
+
+  environment = {
+    systemPackages = [ pkgs.bluez ];
+  };
+
+  services.blueman.enable = true;
+
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
+}
