2b4c76a6d882ba60b15fdf628d4ed4c10cee7ecb
nicochatzi <nico@htz.dev>
2024-02-19 17:29:15 +0100

    only check battery when not on ac

--- .config/i3/check-battery.sh
+++ .config/i3/check-battery.sh
@@ -1,10 +1,11 @@
 while true; do
-    battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+  battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+  is_on_ac=$(cat /sys/class/power_supply/AC/online)
 
-    if [ "$battery_level" -lt 20 ]; then
-        dunstify -u critical -h string:x-dunst-stack-tag:battery "Low Battery" "Battery level is low: ${battery_level}%"
-    fi
+  if [ "$battery_level" -lt 20 ] && [ "$is_on_ac" -eq 0 ]; then
+    dunstify -u critical -h string:x-dunst-stack-tag:battery \
+      "Low Battery" "Battery level is low: ${battery_level}%"
+  fi
 
-    sleep 10
+  sleep 10
 done
-
