e4ee27f84f5cb955973146974e9ea06699f29f33
nicochatzi <nico@htz.dev>
2024-02-19 21:11:04 +0100

    add redshift to nixos

--- .nixfiles/system/desktop.nix
+++ .nixfiles/system/desktop.nix
@@ -65,6 +65,19 @@ in
     lidSwitch = "suspend";
   };
 
+  location.provider = "geoclue2";
+  services.redshift = {
+    enable = true;
+    brightness = {
+      day = "1";
+      night = "1";
+    };
+    temperature = {
+      day = 25000;
+      night = 4000;
+    };
+  };
+
   console.useXkbConfig = true;
 
   users.users.nico = {
