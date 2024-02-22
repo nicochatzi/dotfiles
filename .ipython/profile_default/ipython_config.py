8cb5b1befa1ac6d6a87938e1f5803ac43b3ee1da
nicochatzi <nico@htz.dev>
2024-02-22 19:51:21 +0100

    add ipython customization

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,8 @@
+c.TerminalInteractiveShell.editing_mode = "vi"
+c.TerminalInteractiveShell.shortcuts = [
+    {
+        "command": "IPython:auto_suggest.accept_or_jump_to_end",
+        "new_keys": ["c-z"],
+        "create": True,
+    }
+]
