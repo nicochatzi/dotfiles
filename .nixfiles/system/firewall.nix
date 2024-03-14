724bad51dec4b6637c58a22926b7695f05303b18
nicochatzi <nico@htz.dev>
2024-03-14 13:26:32 +0100

    add kernel security settings

--- .nixfiles/system/firewall.nix
+++ .nixfiles/system/firewall.nix
@@ -13,4 +13,31 @@
     ];
     allowedUDPPorts = [ ];
   };
+
+  # System-wide security settings
+  boot.kernel.sysctl = {
+    # Disable IP forwarding
+    "net.ipv4.ip_forward" = 0;
+    "net.ipv6.conf.all.forwarding" = 0;
+
+    # Disable source packet routing
+    "net.ipv4.conf.all.accept_source_route" = 0;
+    "net.ipv6.conf.all.accept_source_route" = 0;
+
+    # Enable TCP SYN cookies (mitigate SYN flood attack)
+    "net.ipv4.tcp_syncookies" = 1;
+
+    # Enable IP spoofing protection
+    "net.ipv4.conf.all.rp_filter" = 1;
+
+    # Disable ICMP redirects (prevent MITM attacks)
+    "net.ipv4.conf.all.accept_redirects" = 0;
+    "net.ipv6.conf.all.accept_redirects" = 0;
+
+    # Ignore ICMP requests (prevent ping discovery)
+    "net.ipv4.icmp_echo_ignore_all" = 1;
+
+    # Enable logging of spoofed packets, source routed packets, and redirect packets
+    "net.ipv4.conf.all.log_martians" = 1;
+  };
 }
