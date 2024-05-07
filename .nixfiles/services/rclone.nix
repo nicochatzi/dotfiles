da175a345741d01c73294b9d3fdebafea94cc39c
nicochatzi <nico@htz.dev>
2024-05-07 15:59:34 +0200

    launch both proton and gdrive rclone mount services

--- .nixfiles/services/rclone.nix
+++ .nixfiles/services/rclone.nix
@@ -1,5 +1,26 @@
 { pkgs, ... }:
-let mountdir = "/home/nico/cloud/proton";
+let
+  createMountService = name: {
+    description = "Mount ${name} using rclone";
+    after = [ "network-online.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /home/nico/cloud/${name}";
+      ExecStart = ''
+        ${pkgs.rclone}/bin/rclone mount ${name}: /home/nico/cloud/${name} \
+          --log-level NOTICE \
+          --vfs-cache-mode full \
+          --vfs-cache-max-size 500G \
+          --vfs-cache-max-age 336h \
+          --bwlimit-file 16M
+      '';
+      Restart = "always";
+      RestartSec = "10s";
+      Environment = [ "PATH=${pkgs.fuse}/bin:/run/wrappers/bin/:$PATH" ];
+    };
+  };
+
 in {
   environment = {
     systemPackages = [ pkgs.rclone ];
@@ -8,25 +29,9 @@ in {
     '';
   };
 
-  systemd.user.services.rclone-mount = {
-    description = "Mount Proton Drive using rclone";
-    after = [ "network-online.target" ];
-    wantedBy = [ "default.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${mountdir}";
-      ExecStart = ''
-        ${pkgs.rclone}/bin/rclone mount proton: ${mountdir} \
-          --log-level NOTICE \
-          --vfs-cache-mode full \
-          --vfs-cache-max-size 500G \
-          --vfs-cache-max-age 336h \
-          --bwlimit-file 16M
-      '';
-      # ExecStop = "/run/wrappers/bin/fusermount -u ${mountdir}";
-      Restart = "always";
-      RestartSec = "10s";
-      Environment = [ "PATH=${pkgs.fuse}/bin:/run/wrappers/bin/:$PATH" ];
-    };
+  systemd.user.services = {
+    rclone-mount-proton = createMountService "proton";
+    rclone-mount-gdrive = createMountService "gdrive";
   };
 }
+
