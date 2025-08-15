86ecdc7b3026432020c0e057f250014bfbff7a48
nicochatzi <nico@htz.dev>
2025-08-15 21:05:51 +0200

    i3: use ipython for vim bindings

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,4 +1,7 @@
+c = get_config()
 c.TerminalInteractiveShell.editing_mode = "vi"
+c.TerminalInteractiveShell.display_completions = "readlinelike"
+c.TerminalInteractiveShell.confirm_exit = True
 c.TerminalInteractiveShell.shortcuts = [
     {
         "command": "IPython:auto_suggest.accept_or_jump_to_end",
